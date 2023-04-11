2d89e0c23a58d2e9bef54035f764aa969d6dfc87
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-04-11 16:56:28 -0700

    add routes to test shipments schema

--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -9,13 +9,15 @@
 const request = require("supertest");
 const app = require("../app");
 
-
+/**
+ * Tests POST "/"
+ */
 describe("POST /", function () {
   test("valid", async function () {
     // shipProduct
     //   .mockReturnValue(1234);
 
-    
+
     const resp = await request(app).post("/shipments").send({
       productId: 1000,
       name: "Test Tester",
@@ -33,11 +35,51 @@ describe("POST /", function () {
     expect(resp.statusCode).toEqual(400);
   });
 
-  test("throws error if empty request body", async function () {
+  test("fails invalid productId, address", async function () {
     const resp = await request(app)
       .post("/shipments")
-      .send();
+      .send({
+        productId: "1",
+        name: "Name",
+        addr: "Main",
+        zip: "90024"
+      });
     expect(resp.statusCode).toEqual(400);
+    expect(resp.body.error.message).toEqual([
+      "instance.productId is not of a type(s) integer",
+      "instance.addr does not match pattern \"^[0-9]+ [a-zA-Z0-9 ]+$\""
+    ]);
+  });
+
+  test("fails missing name and zip input", async function () {
+    const resp = await request(app)
+      .post("/shipments")
+      .send({
+        productId: 1001,
+        addr: "1234 Main Street"
+      });
+    expect(resp.statusCode).toEqual(400);
+    expect(resp.body.error.message).toEqual([
+      "instance requires property \"name\"",
+      "instance requires property \"zip\""
+    ]);
+  });
+
+  test("fails extra input", async function () {
+    const resp = await request(app)
+      .post("/shipments")
+      .send({
+        productId: 1001,
+        name: "Name",
+        addr: "1234 Main Street",
+        zip: "90024",
+        expedite: "fast!"
+      });
+    expect(resp.statusCode).toEqual(400);
+    expect(resp.body.error.message).toEqual([
+      "instance is not allowed to have the additional property \"expedite\""
+    ]);
   });
 
 });
+
